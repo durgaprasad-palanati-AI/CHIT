6bb387f5beba606def88a64ff553235fd4a4c7be
durgaprasad-palanati-AI <dp.cse5@gmail.com>
2021-05-07 21:00:13 +0530

    delete chit update members holding that chit

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -11,14 +11,14 @@ router.get('/', function (req, res) {
 
 var routecontroller = require('./routecontrol');
 
-router.post('/login',routecontroller.loggin);
-router.get('/viewallchits',authentication.verifyToken,routecontroller.viewallchits);
-router.get('/viewmembers',authentication.verifyToken,routecontroller.viewmembers);
-router.put('/createmembers',authentication.verifyToken,routecontroller.createmembers);
-router.put('/createchit',authentication.verifyToken,routecontroller.createchit);
-router.post('/updatechit',authentication.verifyToken,routecontroller.updatechit);
-router.post('/updatechitpayments',authentication.verifyToken,routecontroller.updatechitpayments);
-router.post('/updateliftedchit',authentication.verifyToken,routecontroller.updateliftedchit);
+router.post('/login',routecontroller.loggin);//
+router.get('/viewallchits',authentication.verifyToken,routecontroller.viewallchits);//
+router.get('/viewmembers',authentication.verifyToken,routecontroller.viewmembers);//
+router.put('/createmembers',authentication.verifyToken,routecontroller.createmembers);//
+router.put('/createchit',authentication.verifyToken,routecontroller.createchit);//
+router.post('/updatechit',authentication.verifyToken,routecontroller.updatechit);//
+router.post('/updatechitpayments',authentication.verifyToken,routecontroller.updatechitpayments);//
+router.post('/updateliftedchit',authentication.verifyToken,routecontroller.updateliftedchit);//
 router.delete('/deletechit',authentication.verifyToken,routecontroller.deletechit);
 
 module.exports = router ;
